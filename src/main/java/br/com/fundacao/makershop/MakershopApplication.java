bc9a248b92e7fefde9d17dd3e845ebcc00b6dad5
Joao Victor Tomaz Lage <58315117+Jao56@users.noreply.github.com>
2024-09-12 15:31:06 -0300

    Add files via upload

---
+++ src/main/java/br/com/fundacao/makershop/MakershopApplication.java
@@ -0,0 +1,13 @@
+package br.com.fundacao.makershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fundacao/makershop/MakershopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fundacao.makershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
